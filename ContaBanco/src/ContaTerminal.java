17a8427db4c6371b3cf24acb3c6ab52a48608d22
HenriqueHatano <henri.koiti2002@gmail.com>
2024-08-06 18:52:04 -0300

    Projeto conta banco finalizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
+    
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+    
+        System.out.println("Qual o seu nome completo?: ");
+        String nomeCompleto = scanner.nextLine();
+
+        System.out.println("Por favor digite o número de sua agência (3 digitos): ");
+        int agencia = scanner.nextInt();
+
+        System.out.println("Digite o número de sua conta (5 digitos): ");
+        int conta = scanner.nextInt();
+
+        double saldo = 1534.50;
+
+        System.out.println("Olá " + nomeCompleto + ", obrigado por criar uma conta em nosso banco. Sua agência é: " + agencia + ", conta: " + conta + " e seu saldo: " + saldo + " já está disponível para saque.");
+
     }
-}
+
+
+    }
+
